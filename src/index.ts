9909b0944b9f5b41f8a61832f3432c0ff4dbac6e
Hasan Ahamed <officials.hasan@gmail.com>
2021-05-08 01:27:51 +0600

    Deployment-12: testing watchtower

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,7 @@ app.use(express.json())
 app.get('/api', (req,res)=>{
     res.status(200).json({
         status: 'success',
+        cicd: 'success',
         database: connectionString || 'n/a',
     })
 })
